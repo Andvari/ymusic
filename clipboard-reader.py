3a8fd9996a8ac0314cf289446a1c5bacd1a29058
Nemo <dedmazai74@mail.ru>
2023-04-16 14:05:14 +0300

    clipboard-reader.py appended

---
+++ clipboard-reader.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from subprocess import run
+from time import sleep
+
+
+with open("download.sh", "w") as f:
+    run(['chmod a+x ./download.sh'], shell=True)
+
+    f.write('#!/bin/sh\n\n')
+    urls = []
+    url_counter = 0
+    while True:
+        url = run(['xsel -b'], shell=True, capture_output=True).stdout.decode('utf-8')
+        url = url[:url.find('feature=share')-1]
+        domain = url[8:25]
+        if domain != 'music.youtube.com':
+            if url_counter != 0:
+                print(domain)
+                print(url_counter)
+                exit()
+        elif url not in urls:
+            f.write(f'echo {url_counter+1}\n')
+            f.write(f'y {url}\npg\n\n')
+            urls.append(url)
+            prev_url = url
+            url_counter += 1
+            print(f'{url_counter} - {url}')
+    
+        sleep(1)
+
+
